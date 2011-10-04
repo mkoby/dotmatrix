079c369b5f32cf86b13d81884b386f039f098ab2
Michael Koby <michael@mkoby.com>
2011-10-04 13:57:20 -0500

    Added rubygems shell script for downloading/installing ruby gems. Needs more work

---
+++ rubygems.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! [ -d ~/Downloads ]; then
+	mkdir -p ~/Downloads
+fi
+
+cd ~/Downloads
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.10.tgz 
+tar xzvf rubygems-1.8.10.tgz 
+rvm ruby rubygems-1.8.10/setup.rb
