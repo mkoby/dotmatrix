38df734fd6ca56da5431cc43c883ef5e25e794ab
Michael Koby <michael@mkoby.com>
2013-12-12 13:22:20 -0600

    Modify git status alias

--- setup.sh
+++ setup.sh
@@ -108,6 +108,6 @@ ln -s `pwd`/paths $PATHS_FILE
 git config --global alias.undo 'reset --soft HEAD^'
 git config --global alias.l "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)%Creset' --abbrev-commit"
 git config --global alias.update '!sh -c "git pull --rebase origin `git symbolic-ref --short -q HEAD`; git fetch origin -p"'
-git config --global alias.s 'status'
+git config --global alias.s '!git status; git submodule status'
 git config --global alias.com 'checkout master'
 git config --global alias.po '!git push origin `git symbolic-ref --short -q HEAD`'
